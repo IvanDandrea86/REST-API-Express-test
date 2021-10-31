c9f1daf42fe300d632bf7c1c4378b209c197e7bc
Ivan D'Andrea <ivan.d.andrea1@gmail.com>
2021-10-31 12:32:31 +0100

    imprev server side

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,34 @@
 async function getJson(url){
-    fetch(url)
+    let data_fetch=await fetch(url)
     .then(response => response.json())
     .then(data => {
         console.log(typeof(data))
         console.table(data)
+        
     })
+    console.log(typeof(data_fetch))
+        console.table(data_fetch)
 }
 async function getData(url){
-    fetch(url)
+    let data_fetch =await fetch(url)
     .then(response => response)
     .then(data => {
         console.log(typeof(data))
         console.table(data.body)
     })
+    console.log(typeof(data_fetch))
+    console.table(data_fetch)
 }
 
 document.getElementById("button_run").addEventListener("click",()=>{
-    getJson('http://localhost:5000/api/')
+    getJson('http://localhost:3000/public/data/db.json')
 })
 document.getElementById("button_run1").addEventListener("click",()=>{
-    getData('http://localhost:5000/2/')
+    getData('http://localhost:3000/public/data/db.json')
 })
 document.getElementById("button_run2").addEventListener("click",()=>{
-    getData('http://localhost:5000/3/')
+    getData('http://localhost:3000/')
 })
 document.getElementById("button_run3").addEventListener("click",()=>{
-    getData('http://localhost:5000/4/')
+    getData('http://localhost:3000/')
 })
--- server.js
+++ server.js
@@ -1,36 +1,32 @@
-var obj = {
-  table: []
-};
-obj.table.push({id: 1, square:2});
-var json = JSON.stringify(obj);
 
-var a=["a","b","c","d"]
-var b=["a","b","c","d"]
+
 
 // create an express app
-const express = require("express")
-const app = express()
-
-app.use(express.json())
+import express from 'express';
+const app=express()
 
 // use the express-static middleware
 app.use(express.static("public"))
-// define the first route
-app.get("/api", function (req, res) {
-  res.send(json);
-  })
-app.get("/2/", function (req, res) {
-    res.send(res);
-    })
-app.get("/3/", function (req, res) {
-      res.send(b);
-      })
-app.get("/4/", function (req, res) {
-        res.send("<h1>i/h1>");
-        })  
 
-// start the server listening for requests
-app.listen(process.env.PORT || 5000, 
-	() => console.log("Server is running...at port 5000"));
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+  });
+
+ 
+  app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+  });
+   
+  app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+  });
+   
+  app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+  });
+   
+  app.listen(process.env.PORT || 3000, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`),
+  );
 
   
